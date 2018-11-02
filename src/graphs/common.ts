7167cf8645a7a036ea63f2c0c7439f969f367fb2
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2018-11-02 16:46:27 +0100

    fix parsing CSV to graph
    An additional vertex was added if the file had
    a trailing empty line.

--- src/graphs/common.ts
+++ src/graphs/common.ts
@@ -4,5 +4,7 @@ export function csvToMatrix(data: string): Matrix {
   return data
     .replace(/\\r/g, '')
     .split('\n')
+    .map(row => row.trim())
+    .filter(row => row !== '')
     .map(row => row.split(',').map(element => parseInt(element, 10)));
 }
